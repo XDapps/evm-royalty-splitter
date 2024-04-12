fa0ec106abca26b4dbf4c4dcc706b7501399303c
XDapps <xdev@xdapps.xyz>
2024-04-12 11:47:16 -0400

    Clean Up

--- contracts/SplitRoyalty.sol
+++ contracts/SplitRoyalty.sol
@@ -1,7 +1,6 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity 0.8.24;
 
-import "hardhat/console.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
@@ -9,7 +8,6 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract SplitRoyalty {
-	
     event RoyaltyReceived(address indexed from, uint256 amount, uint256 indexed uid);
     event RoyaltyPaidNative(address indexed to, uint256 amount, uint256 indexed uid);
     event RoyaltyPaidERC20(address indexed token, address indexed to, uint256 amount, uint256 indexed uid);
@@ -18,64 +16,62 @@ contract SplitRoyalty {
         address payable royaltyAddress;
         uint256 royalty;
     }
-     
+
     Royalty[] public owners;
 
-    constructor(address payable[] memory _publishers, uint256[] memory _royaltyPercentage){
+    constructor(address payable[] memory _publishers, uint256[] memory _royaltyPercentage) {
         uint totalPercentage = 0;
-        for (uint i=0; i<_publishers.length; i++) {
-            Royalty memory newRoyalty = Royalty(_publishers[i], _royaltyPercentage[i]); 
+        for (uint i = 0; i < _publishers.length; i++) {
+            Royalty memory newRoyalty = Royalty(_publishers[i], _royaltyPercentage[i]);
             totalPercentage = totalPercentage + newRoyalty.royalty;
             owners.push(newRoyalty);
         }
-        require(totalPercentage == 10000, 'Total Percentage must equal 10,000');
-        }
+        require(totalPercentage == 10000, "Total Percentage must equal 10,000");
+    }
 
-    function _distributeRoyalty(uint256 loopIndex, uint256 _amountReceived, uint256 _uid) internal{
-		console.log("_distributeRoyalty");
-            Royalty storage royalty = owners[loopIndex];
-            uint256 percentage = royalty.royalty;
-            address payable addressToPay = royalty.royaltyAddress;
-		console.log("addressToPay");
-            uint256 amountToPay = (percentage * _amountReceived)/10000;
-            bool wasSentRoyalty = addressToPay.send(amountToPay);
-            require(wasSentRoyalty, "Failed to send Ether");
+    function _distributeRoyalty(uint256 loopIndex, uint256 _amountReceived, uint256 _uid) internal {
+        Royalty storage royalty = owners[loopIndex];
+        uint256 percentage = royalty.royalty;
+        address payable addressToPay = royalty.royaltyAddress;
+        uint256 amountToPay = (percentage * _amountReceived) / 10000;
+        bool wasSentRoyalty = addressToPay.send(amountToPay);
+        require(wasSentRoyalty, "Failed to send Ether");
         emit RoyaltyPaidNative(addressToPay, amountToPay, _uid);
-   }
-    function _distributeRoyaltyERC20(address _tokenAddress, uint256 loopIndex, uint256 _amountReceived, uint256 _uid) internal{
-            Royalty storage royalty = owners[loopIndex];
-            uint256 percentage = royalty.royalty;
-            address payable addressToPay = royalty.royaltyAddress;
-            uint256 amountToPay = (percentage * _amountReceived)/10000;
-			IERC20 token = IERC20(_tokenAddress);
-			token.transfer(addressToPay, amountToPay);
-			emit RoyaltyPaidERC20(_tokenAddress, addressToPay, amountToPay, _uid);
-   }
+    }
+    function _distributeRoyaltyERC20(
+        address _tokenAddress,
+        uint256 loopIndex,
+        uint256 _amountReceived,
+        uint256 _uid
+    ) internal {
+        Royalty storage royalty = owners[loopIndex];
+        uint256 percentage = royalty.royalty;
+        address payable addressToPay = royalty.royaltyAddress;
+        uint256 amountToPay = (percentage * _amountReceived) / 10000;
+        IERC20 token = IERC20(_tokenAddress);
+        token.transfer(addressToPay, amountToPay);
+        emit RoyaltyPaidERC20(_tokenAddress, addressToPay, amountToPay, _uid);
+    }
 
-    function receiveRoyalty(uint256 amountReceived, uint256 uid) payable public{
-		console.log("receiveRoyaltyyy");
-		console.log("owners.length ", owners.length);
+    function receiveRoyalty(uint256 amountReceived, uint256 uid) public payable {
         require(msg.value >= amountReceived);
         emit RoyaltyReceived(msg.sender, amountReceived, uid);
-        for (uint i=0; i<owners.length; i++) {
-		console.log("for");
+        for (uint i = 0; i < owners.length; i++) {
             _distributeRoyalty(i, amountReceived, uid);
         }
     }
-    function distributeERC20RoyaltyTokenBalence(address _tokenAddress) external{
-		IERC20 token = IERC20(_tokenAddress);
-		uint256 balance = token.balanceOf(address(this));
-		require(balance > 0, "No balance to distribute");
-        for (uint i=0; i<owners.length; i++) {
+    function distributeERC20RoyaltyTokenBalence(address _tokenAddress) external {
+        IERC20 token = IERC20(_tokenAddress);
+        uint256 balance = token.balanceOf(address(this));
+        require(balance > 0, "No balance to distribute");
+        for (uint i = 0; i < owners.length; i++) {
             _distributeRoyaltyERC20(_tokenAddress, i, balance, 0);
         }
     }
-	receive() external payable{ 
-		console.log("Received ether");
-		receiveRoyalty(msg.value, 0);
-	 }
-	fallback() external payable { 
-		console.log("Fallback ether");
-		receiveRoyalty(msg.value, 0);
-	 }
+    receive() external payable {
+        receiveRoyalty(msg.value, 0);
+    }
+    fallback() external payable {
+        receiveRoyalty(msg.value, 0);
+    }
 }
